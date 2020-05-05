983e5c1b59c1963c76db0e5277b7b3b3abb2ff01
khadija267 <37675645+khadija267@users.noreply.github.com>
2020-05-06 01:50:37 +0200

    Update reflection.py

--- reflection.py
+++ reflection.py
@@ -18,32 +18,21 @@ def build_reflection_matrix(basis) : # The parameter basis is a 2×2 matrix that
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
-
-# This is the matrix of Bear's basis vectors.
-# (When you've done the exercise once, see what happns when you change Bear's basis.)
+#basis can be changed
 bearBasis = np.array(
     [[1,   -1],
      [1.5, 2]])
-# This line uses your code to build a transformation matrix for us to use.
 T = build_reflection_matrix(bearBasis)
-
-# Bear is drawn as a set of polygons, the vertices of which are placed as a matrix list of column vectors.
-# We have three of these non-square matrix lists: bear_white_fur, bear_black_fur, and bear_face.
-# We'll make new lists of vertices by applying the T matrix you've calculated.
 reflected_bear_white_fur = T @ bear_white_fur
 reflected_bear_black_fur = T @ bear_black_fur
 reflected_bear_face = T @ bear_face
-
-# This next line runs a code to set up the graphics environment.
+#set up the graphics environment from fns.py file
 ax = draw_mirror(bearBasis)
-
-# We'll first plot Bear, his white fur, his black fur, and his face.
+#plot the bear
 ax.fill(bear_white_fur[0], bear_white_fur[1], color=bear_white, zorder=1)
 ax.fill(bear_black_fur[0], bear_black_fur[1], color=bear_black, zorder=2)
 ax.plot(bear_face[0], bear_face[1], color=bear_white, zorder=3)
-
-# Next we'll plot Bear's reflection.
+#reflection plot
 ax.fill(reflected_bear_white_fur[0], reflected_bear_white_fur[1], color=bear_white, zorder=1)
 ax.fill(reflected_bear_black_fur[0], reflected_bear_black_fur[1], color=bear_black, zorder=2)
 ax.plot(reflected_bear_face[0], reflected_bear_face[1], color=bear_white, zorder=3);
-    
